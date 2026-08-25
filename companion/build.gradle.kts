d8babe4484a88753ca7b49c9a28383c14688efb4
daplevat67-coder <daplevat67@gmail.com>
2026-08-25 17:06:51 +0300

    Bump Companion Android to v0.9.9

--- companion/build.gradle.kts
+++ companion/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = storeApplicationId
         minSdk = 26
         targetSdk = 36
-        versionCode = 21
-        versionName = "0.9.8"
+        versionCode = 22
+        versionName = "0.9.9"
     }
 }
 
