5a39deeba0206542f8fbf54ca14a912e0e5edf63
daplevat67-coder <daplevat67@gmail.com>
2026-08-27 20:25:32 +0300

    Bump app to v0.9.11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = storeApplicationId
         minSdk = 26
         targetSdk = 36
-        versionCode = 23
-        versionName = "0.9.10"
+        versionCode = 24
+        versionName = "0.9.11"
     }
 }
 
